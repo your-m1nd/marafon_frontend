9eb516059fa6b3b5cdd65c53c057b6f516aeb1cc
your-m1nd <vi.neurogirl@gmail.com>
2023-06-06 21:30:10 +0300

    add script and change html

--- script/script.js
+++ script/script.js
@@ -24,3 +24,54 @@ for (let i=0; i<imagePaths.length; i++) {
     imagePaths[i].src = imageCats[i];
     imagePaths[i].alt = nameCats[i];
 }
+
+const infoCats = [
+    { 
+        type: "Гладкошерстный", 
+        color: "Белый с рыжими глазами", 
+        vaccine: "Вакцинирована",
+        passport: "Есть ветпаспорт",
+        things: "Приучен к когтеточке",
+        tray: "Знает лоточек"
+    },
+    { 
+        type: "Гладкошерстный", 
+        color: "Трёхцветная с зелеными глазами", 
+        vaccine: "Вакцинирована",
+        passport: "Есть ветпаспорт",
+        things: "Приучен к когтеточке",
+        tray: "Знает лоточек"
+    },
+    { 
+        type: "Гладкошерстный", 
+        color: "Черно-белый с зелеными глазами", 
+        vaccine: "Вакцинирована",
+        passport: "Есть ветпаспорт",
+        things: "Приучен к когтеточке",
+        tray: "Знает лоточек"
+    },
+    { 
+        type: "Гладкошерстный", 
+        color: "Трёхцветная с зелеными глазами", 
+        vaccine: "Вакцинирована",
+        passport: "Есть ветпаспорт",
+        things: "Приучен к когтеточке",
+        tray: "Знает лоточек"
+    },
+];
+
+for (let i = 0; i < infoCats.length; i++) {
+    const cat = infoCats[i];
+    const infoList = `
+      <ul>
+        <li>${cat.type}</li>
+        <li>${cat.color}</li>
+        <li>${cat.vaccine}</li>
+        <li>${cat.passport}</li>
+        <li>${cat.things}</li>
+        <li>${cat.tray}</li>
+      </ul>
+    `;
+    const targetElement = document.querySelector(`#name-cat-${i+1}`);
+    targetElement.insertAdjacentHTML('afterend', infoList);
+}
